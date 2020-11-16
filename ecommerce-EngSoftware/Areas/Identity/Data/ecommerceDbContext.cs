678c107726ed6f4d1dde16288fd1f21152e1a502
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2020-11-16 20:44:49 -0300

    Identity Scaffold -> For Users Account

---
+++ ecommerce-EngSoftware/Areas/Identity/Data/ecommerceDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ecommerce_EngSoftware.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_EngSoftware.Data
+{
+    public class ecommerceDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ecommerceDbContext(DbContextOptions<ecommerceDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ecommerce-EngSoftware/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ecommerce_EngSoftware.Areas.Identity.Data;
+using ecommerce_EngSoftware.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ecommerce_EngSoftware.Areas.Identity.IdentityHostingStartup))]
+namespace ecommerce_EngSoftware.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ecommerceDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ecommerceDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ecommerceDbContext>();
+            });
+        }
+    }
+}
